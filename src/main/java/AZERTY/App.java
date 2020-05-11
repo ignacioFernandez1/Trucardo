977c32f93873b79bcc591cd879460f2bdee8b062
nacho <nachofnz22@gmail.com>
2020-05-11 20:08:49 -0300

    Se agrego funcionalidad de ventana

--- src/main/java/AZERTY/App.java
+++ src/main/java/AZERTY/App.java
@@ -1,19 +1,28 @@
 package AZERTY;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App {
+    private JButton button1;
+    private JPanel panel1;
+
+    public App() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                JOptionPane.showMessageDialog(null, "Trucardopolis!!!!!!");
+            }
+        });
+    }
+
+    public static void main(String[] args )
     {
-        Mazo mazo = new Mazo();
-        System.out.println(mazo.toString());
-        System.out.println(mazo.getMazo().size());
-        mazo.mezclar();
-        System.out.println("-----------------------------------------");
-        System.out.println(mazo.toString());
-        System.out.println(mazo.getMazo().size());
+        JFrame frame = new JFrame("Trucardo");
+        frame.setContentPane(new App().panel1);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
     }
 }
