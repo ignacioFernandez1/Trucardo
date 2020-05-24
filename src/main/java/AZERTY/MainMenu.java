52ae13faeade9cad1c31f7b8a535ec693f3eca2c
nacho <nachofnz22@gmail.com>
2020-05-24 19:42:12 -0300

    GUI arreglando fondo

--- src/main/java/AZERTY/MainMenu.java
+++ src/main/java/AZERTY/MainMenu.java
@@ -4,37 +4,36 @@ import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class MainMenu extends JFrame implements ActionListener{
     //private JtextField name;
     private Font fuente;
     private JLabel background;
+    private JLabel background_img;
     private JButton boton;
     private JLabel pika;
 
     public MainMenu(){
         setLayout(null);
-        JLabel background;
-        setTitle("Aguante IntelliJ viejo");
+        setTitle("Trucardo");
         this.fuenteMinecraft();
-
-        ImageIcon pikachu = new ImageIcon("images/PIKA.gif");
-        pika = new JLabel(pikachu);
-        pika.setBounds(0,0,300,225);
-        //add(pika);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         ImageIcon back = new ImageIcon("images/GIFARDO.gif");
         background = new JLabel(back);
         background.setBounds(0,0,640,960);
         add(background);
 
+        ImageIcon back_img = new ImageIcon("images/MENUU.png");
+        background_img = new JLabel(back_img);
+        background_img.setBounds(0,0,640,960);
+        add(background_img);
 
-        ImageIcon img = new ImageIcon("images/MENU.png");
         boton = new JButton("JUGAR");
         boton.setHorizontalTextPosition(boton.CENTER);
         boton.setVerticalTextPosition(boton.CENTER);
         boton.setBounds(320-90,480-25,100,30);
-        boton.setIcon(img);
         boton.setBackground(new Color(95,255, 80));
         boton.setFont(this.fuente);
         boton.setForeground(new Color(50,50,50));
@@ -67,5 +66,12 @@ public class MainMenu extends JFrame implements ActionListener{
         menu.setVisible(true);
         menu.setLocationRelativeTo(null);
         menu.setResizable(false);
+        try {
+            TimeUnit.SECONDS.sleep(4);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        menu.background.setVisible(false);
+
     }
 }
