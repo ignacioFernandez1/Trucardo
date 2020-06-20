5d954f87090b95b0f1adda595c186cb988baeaa4
nacho <nachofnz22@gmail.com>
2020-06-20 13:23:37 -0300

    agregue otro hilo para tomar decision con pausa

--- src/main/java/AZERTY/Task.java
+++ src/main/java/AZERTY/Task.java
@@ -14,7 +14,7 @@ public class Task implements Runnable{
     @Override
     public void run() {
         try {
-            Thread.sleep(Random);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
